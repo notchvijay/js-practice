50cbf515a3e80a298e048dfefc9c1724ff335b34
notchvijay <krishnaxnotch@gmail.com>
2026-01-22 11:23:27 +0000

    comparison

---
+++ basic01/5comparasion.js
@@ -0,0 +1,15 @@
+console.log(2>1)
+console.log(3 >= 3)
+console.log(6==5)
+console.log(3!=5)
+
+console.log("5">4)
+console.log("6"==6)
+
+let num1=4 , num2=5
+console.log((num1)>(num2));
+
+/* in java script the comparison work diff */
+// === strict check
+
+console.log("5"===5)
