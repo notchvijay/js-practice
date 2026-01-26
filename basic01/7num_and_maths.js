04dbc50a76df3ec6f04e243a488fcf9ca4549934
notchvijay <krishnaxnotch@gmail.com>
2026-01-26 08:26:37 +0000

    math and num

--- basic01/7num_and_maths.js
+++ basic01/7num_and_maths.js
@@ -1,17 +1,34 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(3))
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3))
 
 const newnumber=1412.58669
-console.log(newnumber.toPrecision(3));
+// console.log(newnumber.toPrecision(3));
 
 const Hundreds=10000000
-console.log(Hundreds.toLocaleString('en-In'));
+// console.log(Hundreds.toLocaleString('en-In'));
 
 //+++++++++++++++++++++++++++++++++  Maths  ++++++++++++++++++++++++++++++++++++++++++
 
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.4))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(3,4,5,1));
+// console.log(Math.max(4,5,6,55));
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log((Math.random()*10)+1)
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
