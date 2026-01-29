2bb769e2342f7b72c252aa4e38bcf8b00a23d8a0
notch <krishnaxnotch@gmail.com>
2026-01-29 10:32:18 +0000

    array basic

--- basic02/01array.js
+++ basic02/01array.js
@@ -3,25 +3,25 @@
 const myArr= [0,1,2,3,4,5];
 const myHeros=["kaal","shaktiman","balveer"]
 
-// console.log(myArr[3]);
-// console.log(myHeros[0]);
+console.log(myArr[3]);
+console.log(myHeros[0]);
 
-// array method 
-// myArr.push(6);
-// myArr.push(7);
-// myArr.pop();
+array method 
+myArr.push(6);
+myArr.push(7);
+myArr.pop();
 
-// myArr.unshift(9);
-// myArr.shift()
+myArr.unshift(9);
+myArr.shift()
 
-// console.log(myArr.includes(5))
-// console.log(myArr.indexOf(4))
+console.log(myArr.includes(5))
+console.log(myArr.indexOf(4))
 
 const newArr=myArr.join();
 
-// console.log(myArr);
-// console.log(newArr);
-// console.log(typeof newArr);
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr);
 
 
 // slice or splice
