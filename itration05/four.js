4ff14f438d1a23455fcbbb9979aa3b45ceffab97
notch <krishnaxnotch@gmail.com>
2026-02-04 12:03:56 +0530

    for in

---
+++ itration05/four.js
@@ -0,0 +1,15 @@
+const lang={
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+
+for (const key in lang) {
+//   console.log(`${key} :- ${lang[key]}`)
+}
+
+const proograming=["js","java","cpp","rb"];
+for(const key in proograming){
+    console.log(`${key}:-${proograming[key]}`)
+}
